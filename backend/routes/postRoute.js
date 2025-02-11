158d50d70ff82dc4787f260e198332e828d0e083
TejaBudumuru3 <tejabudumuru3@gmail.com>
2025-02-11 16:06:36 +0530

    11-02-2025

---
+++ backend/routes/postRoute.js
@@ -0,0 +1,34 @@
+const express = require("express")
+require('dotenv').config({ path: '../myenv.env' })
+const axios = require('axios')
+const Groq = require('groq-sdk')
+const route = express.Router()
+
+route.post("/generate", async (req, res) => {
+    const { input } = req.body;
+
+    if (!input) {
+        return res.status(400).json({ error: "Input text is required" });
+    }
+
+    try {
+        console.clear();
+        console.log(`${process.env.GROQ_API}`);
+        const groq = new Groq({ apiKey: `${process.env.GROQ_API}`});
+        const response = await groq.chat.completions.create({
+            messages:[{
+                role:"user",
+                content:`${input}`,
+            },],
+            model: "gemma2-9b-it",
+        })
+        console.log(response.choices[0].message.content);
+        res.status(200).json({response:response.choices[0].message.content});
+        // res.send(response.choices[0].message.content);
+    } catch (error) {
+        console.error("Error generating post:", error.response?.data || error.message);
+        res.status(500).json({ error: "Failed to generate post" });
+    }
+});
+
+module.exports = route
--- backend/scripts.js
+++ backend/scripts.js
@@ -1,1 +1,19 @@
-require("dotenv").config;
+require("dotenv").config()
+const express = require("express")
+const axios = require("axios")
+const route = require("./routes/postRoute")
+
+const app = express()
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+app.get("/",(req,res)=>{
+    res.send("server is running")
+})
+
+app.use("/",route)
+
+app.listen(PORT,()=>{
+    console.log(`server started on ${PORT} port`)
+})
