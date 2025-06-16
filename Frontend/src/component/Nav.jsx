1353b1e3d7249d11fa6dbd7cede482cea6fb9938
TejaBudumuru3 <tejabudumuru3@gmail.com>
2025-06-16 10:55:51 +0530

    feat: enable navigation to home on title click in NavBar component

--- Frontend/src/component/Nav.jsx
+++ Frontend/src/component/Nav.jsx
@@ -1,15 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
 
+import { useNavigate } from "react-router-dom";
 const NavBar = ({children})=>{
 
-    
+    const Nav = useNavigate();
 
     // console.log("Userdata from Nav",userData)
     return( 
         <>
         <nav className="Navbar" style={{display: "flex", justifyContent: "space-between", alignItems: "center", padding: "10px 20px", backgroundColor: "rgb(10,10,10"}}>
             <div className="title">
-                <h1 className="title-text">Post Generator</h1>
+                <h1 className="title-text" onClick={()=>{Nav("/home")}}>Post Generator</h1>
             </div>
             {children}
         </nav> 
