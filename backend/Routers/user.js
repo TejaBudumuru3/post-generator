ce5a74e7a4b1bb2f683b9cc38c3069d9bdc916d5
TejaBudumuru3 <tejabudumuru3@gmail.com>
2025-06-17 12:54:09 +0530

    refactor: update SameSite attribute for cookie in signin route to 'lax'

--- backend/Routers/user.js
+++ backend/Routers/user.js
@@ -76,7 +76,7 @@ UserRouter.post("/signin", async function (req, res) {
 
     res.cookie("token",token,{
       httpOnly:true,
-      sameSite:"none",
+      sameSite:"lax",
       secure:false,
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
