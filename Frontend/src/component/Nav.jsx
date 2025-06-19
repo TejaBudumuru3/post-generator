4434a0ec357cfae9fc87cdd80c0c70d3629bc104
TejaBudumuru3 <tejabudumuru3@gmail.com>
2025-06-19 19:19:58 +0530

    refactor: update title click handler to navigate based on user authentication

--- Frontend/src/component/Nav.jsx
+++ Frontend/src/component/Nav.jsx
@@ -1,14 +1,23 @@
-
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 const NavBar = ({children})=>{
-
+    const user = useSelector((state)=> {return state.user.data})
     const Nav = useNavigate();
+    function titleHandler(){
+        if(user){
+            Nav("/home")
+        }
+        else{
+            Nav("/")
+        }
+    }
 
     return( 
         <>
         <nav className="Navbar">
             <div className="title">
-                <h1 className="title-text" onClick={()=>{Nav("/")}}>Post Generator</h1>
+                <h1 className="title-text" onClick={titleHandler}>Post Generator</h1>
             </div>
             {children}
         </nav> 
