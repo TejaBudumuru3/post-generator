393be96552c0023750ddded51f63c05403d6b666
TejaBudumuru3 <tejabudumuru3@gmail.com>
2025-07-03 17:34:28 +0530

    Remove page reload on login redirection and use useNavigate for navigation

--- Frontend/src/component/Login.jsx
+++ Frontend/src/component/Login.jsx
@@ -57,7 +57,7 @@ const Login = ({onClose}) => {
 
                 setTimeout(()=>{
                   // onClose();
-                  window.location.reload();
+                  // window.location.reload();
                   Navigate("/");
                 },2000)
 
