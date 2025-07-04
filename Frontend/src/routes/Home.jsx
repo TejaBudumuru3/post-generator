02bc4bb8f1e679283a4105c5c642ac34f65dd4b7
TejaBudumuru3 <tejabudumuru3@gmail.com>
2025-07-04 20:04:00 +0530

    Refactor Home component by removing unused state and effect hooks for cleaner code

--- Frontend/src/routes/Home.jsx
+++ Frontend/src/routes/Home.jsx
@@ -4,19 +4,19 @@ import PostSection from '../component/PostSection.jsx'
 import { useSelector } from 'react-redux'
 import Signup from "../component/Signup.jsx";
 import ProfileWrapper from "../component/ProfileWrapper";
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 
 const Home = () => {
   const userData = useSelector((state)=> state.user.data);
-  const [loadPost, setLoadPost] = useState(true);
-  const posts = useSelector((state)=>state.posts.item)
-  const linkedinPost = useSelector((state)=>state.linkedinPost)
+  // const [loadPost, setLoadPost] = useState(true);
+  // const posts = useSelector((state)=>state.posts.item)
+  // const linkedinPost = useSelector((state)=>state.linkedinPost)
   
-  useEffect(()=>{
-  if((posts === undefined || posts === null || Object.keys(posts).length === 0) && (linkedinPost === null || linkedinPost === "")) {
-    setLoadPost(false)
+  // useEffect(()=>{
+  // if((posts === undefined || posts === null || Object.keys(posts).length === 0) && (linkedinPost === null || linkedinPost === "")) {
+  //   setLoadPost(false)
     
-  }},[loadPost])
+  // }},[loadPost])
   
   return ( 
     <>
@@ -26,17 +26,17 @@ const Home = () => {
             </div>
       </NavBar>
      
-        <>
+        
           
-          <div className="main-section">
-              <InputField/>
-            {loadPost && 
-              <div className="post-section">
-                <PostSection/>
-              </div>
-            }
+      <div className="main-section">
+          <InputField/>
+        
+          <div className="post-section">
+            <PostSection/>
           </div>
-        </>
+        
+      </div>
+        
     </>
   )
 }
