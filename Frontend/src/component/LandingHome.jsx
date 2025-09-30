cc5c65561e8e66471ac96323251aa13b010e3889
TejaBudumuru3 <tejabudumuru3@gmail.com>
2025-09-30 20:55:59 +0530

    tailwind set

--- Frontend/src/component/LandingHome.jsx
+++ Frontend/src/component/LandingHome.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { Card } from "./Card.jsx";
-import "tailwindcss";
+// import "tailwindcss";
 const LandPage = () => {
   const Nav = useNavigate();
 
@@ -8,7 +8,7 @@ const LandPage = () => {
     <>
   
 
-      <div className="text-white text-center mt-10 text-4xl font-bold">
+      <div className="text-white text-center mt-10 text-8xl font-bold bg-black">
         <p className="text-4xl bg-black">© 2025 AInfinity. All rights reserved.</p>
         <p>code at github</p>
       </div>
