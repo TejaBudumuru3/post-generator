06aef7207356106db094b09b931cc0af5e32f604
Vamsi_0 <iamvamsi0@gmail.com>
2025-10-03 20:24:39 +0530

    Intial commit

--- Frontend/src/routes/Home.jsx
+++ Frontend/src/routes/Home.jsx
@@ -28,6 +28,8 @@ const Home = () => {
         <NavBar >
           <div className="user">
                   {userData ? (<ProfileWrapper user={userData}/>) : (<Signup />)}
+
+                  
               </div>
         </NavBar>
 
