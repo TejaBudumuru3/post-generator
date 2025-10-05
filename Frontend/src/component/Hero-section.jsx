3d077103cd152bb0096cc0edb8187886a866e426
TejaBudumuru3 <tejabudumuru3@gmail.com>
2025-10-05 21:28:21 +0530

    updated image in hero section

--- Frontend/src/component/Hero-section.jsx
+++ Frontend/src/component/Hero-section.jsx
@@ -25,8 +25,8 @@ const Hero = () => {
       <p className="text-lg md:text-2xl md:text-left px-3">AI POWERED /CONTENT GENREATION </p>
       </div>
       <div className="flex-0  w-full h-6/10 md:w-7/10 md:h-[65%] md:relative md:right-0 content-center lg:right-20">
-        <img src="pro.png" className="w-full h-full object-cover "/>
-        <p className="absolute md:left-2 md:text-md lg:top-[36%] lg:left-[-20%] left-2 top-[40%] text-white text-[8px] lg:text-xl md:top-[40%]">[USER PROMPT]</p>
+        <img src="pro.png" className="w-full h-full object-cover top-[100%]"/>/
+        {/* <p className="absolute md:left-2 md:text-md lg:top-[36%] lg:left-[-20%] left-2 top-[20%] text-white text-[8px] lg:text-xl md:top-[40%]">[USER PROMPT]</p> */}
       </div>
       <div className="flex-2  lg:h-32 flex px-3 md:gap-5 md:justify-normal px-3 items-center justify-between  w-full">
         <button className="p-2 bg-[#36b6ea] h-10 rounded text-white hover:scale-110 hover:duration-700">Join Community</button>
