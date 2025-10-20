6e2cbe824b6883e0514643f595f20b84b05f9082
TejaBudumuru3 <tejabudumuru3@gmail.com>
2025-10-20 21:40:13 +0530

    fixing build error

--- new-fe/src/components/Toast.tsx
+++ new-fe/src/components/Toast.tsx
@@ -1,27 +1,18 @@
-import {useEffect,useRef} from 'react';
 
 
-const Toast = ({state,message}:any)=>{
+const Toast = ()=>{
 
-    const toastRef = useRef(null);
-
-    useEffect(() => {
-        const toastElement = toastRef.current;
-        if (toastElement) {
-            const toast = new bootstrap.Toast(toastElement);
-            toast.show();
-        }
-    }, []);
+    
 
     return (<>
-    <div ref={toastRef} className={`toast fixed top-2 right-0 md:right-2 align-items-center text-bg-${state} border-0 margin-top-0`} role="alert" aria-live="assertive" aria-atomic="true" style={{zIndex:100}} >
+    {/* <div ref={toastRef} className={`toast fixed top-2 right-0 md:right-2 align-items-center text-bg-${state} border-0 margin-top-0`} role="alert" aria-live="assertive" aria-atomic="true" style={{zIndex:100}} >
         <div className="d-flex">
             <div className="toast-body">
             {message}
             </div>
             <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
-    </div>
+    </div> */}
 
     </>)
 }
